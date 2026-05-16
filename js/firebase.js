ade621ff3ea21f138fe724aa3909438218b3dad3
kawauso142525-crypto <kawauso142525@gmail.com>
2026-05-17 02:16:57 +0900

    multi file support

--- js/firebase.js
+++ js/firebase.js
@@ -1,60 +1,35 @@
-async function loadFile(fileName) {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-  const allFiles = loadAllFiles();
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  deleteDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-  if (allFiles[fileName]) {
-    return allFiles[fileName];
-  }
+const firebaseConfig = {
+  apiKey: "AIzaSyC-Qon2VZv-heyBTBYLCrpwC_C5MDMrNws",
+  authDomain: "study-mri.firebaseapp.com",
+  projectId: "study-mri",
+  storageBucket: "study-mri.firebasestorage.app",
+  messagingSenderId: "231073050806",
+  appId: "1:231073050806:web:7d68cdc26e940b54cd7617"
+};
 
-  const { doc, getDoc } = window.firebaseFunctions;
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-  const docRef = doc(
-    window.firebaseDB,
-    "files",
-    fileName
-  );
+/*
+  グローバル公開（他JSから使うため）
+*/
+window.firebaseDB = db;
 
-  const docSnap = await getDoc(docRef);
+window.firebaseFunctions = {
+  doc,
+  setDoc,
+  getDoc,
+  deleteDoc
+};
 
-  if (docSnap.exists()) {
-    const data = docSnap.data().tableData;
-
-    allFiles[fileName] = data;
-    saveAllFiles(allFiles);
-
-    return data;
-  }
-
-  return null;
-}
-
-async function saveFile(fileName, data) {
-
-  const allFiles = loadAllFiles();
-
-  allFiles[fileName] = data;
-
-  saveAllFiles(allFiles);
-
-  const { doc, setDoc } = window.firebaseFunctions;
-
-  await setDoc(
-    doc(window.firebaseDB, "files", fileName),
-    { tableData: data }
-  );
-}
-
-async function deleteFile(fileName) {
-
-  const allFiles = loadAllFiles();
-
-  delete allFiles[fileName];
-
-  saveAllFiles(allFiles);
-
-  const { doc, deleteDoc } = window.firebaseFunctions;
-
-  await deleteDoc(
-    doc(window.firebaseDB, "files", fileName)
-  );
-}
+console.log("Firebase initialized:", db);
